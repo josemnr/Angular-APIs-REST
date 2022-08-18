7d958836af39c628b6cb2e14f7bfdfc55594d0d7
Pepe <pepe@MacBook-Pro-de-Pepe.local>
2022-08-18 13:42:36 -0500

    Add upload and download functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { User } from './models/user.model';
 
 import { AuthService } from './services/auth.service'
 import { UsersService } from './services/users.service'
+import { FilesService } from './services/files.service';
 
 
 @Component({
@@ -15,10 +16,12 @@ export class AppComponent {
   showImg = true;
   token = '';
   profile: User | null = null;
+  imgRta = '';
 
   constructor(
+    private authService: AuthService,
+    private filesService: FilesService,
     private usersService: UsersService,
-    private authService: AuthService
   ) {
 
   }
@@ -56,5 +59,21 @@ export class AppComponent {
     .subscribe(user => {
       this.profile = user;
     })
+  }
+
+  downloadPdf() {
+    this.filesService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
+    .subscribe()
+  }
+
+  onUpload(event: Event) {
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0);
+    if(file) {
+      this.filesService.uploadFile(file)
+      .subscribe(rta => {
+        this.imgRta = rta.location;
+      });
+    }
   }
 }
